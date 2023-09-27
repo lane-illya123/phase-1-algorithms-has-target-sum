262609c3af5e585a28d6cd7562efb8387b121dfe
illyalane <lane_illya@yahoo.com>
2023-09-27 00:02:43 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
+//Runtime O(n)
 function hasTargetSum(array, target) {
-  // Write your algorithm here
-}
+// create an object that stores seen numbers
+  const seenNumbers = {}
+
+// Iterates thur each number in the array
+  for(let i = 0; i < array.length; i++){
+
+    const compliment = target - array[i];
+
+    if(seenNumbers[compliment]) return true;
+
+    seenNumbers[array[i]] = true
+  }
+
+  return false
+
+  }
 
 /* 
   Write the Big O time complexity of your function here
-*/
+  O(n)
+  */
 
 /* 
   Add your pseudocode here
-*/
+([3, 8, 12, 4, 11, 7], 10)
+  ^  ^                  ^
+  N  N2               Target
+
+after N2 full cycle 
+
+([3, 8, 12, 4, 11, 7], 10)
+     ^   ^              ^
+     N  N2            Target
+
+
+  */
 
 /*
   Add written explanation of your solution here
-*/
+sort thur an array while testing the items inside to match the target number
+  */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
